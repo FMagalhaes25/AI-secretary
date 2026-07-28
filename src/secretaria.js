4e86d33579ba558cd7a899064e5fcfbbfb07cf81
Felipe Vitalis <felipe.mostaco@vitalisinvest.com.br>
2026-07-28 19:56:56 -0300

    first commit

---
+++ src/secretaria.js
@@ -0,0 +1,65 @@
+import {GoogleGenAI} from '@google/genai';
+
+const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GEN_AI_API_KEY });
+
+const contents = [
+    {
+        role: "user",
+        parts: [{ text: "qual a temperatura no Brasil?" }]
+    }
+];
+
+var response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: contents,
+    config: {
+        tools: [
+            {
+                functionDeclarations: [
+                    {name: "getTodayDate", description: "retorna a data de hoje no formato yyyy-mm-dd"},
+                    {   
+                        name: "getCountryTemperature",
+                        description: "Retorna a temperatura do país indicado",
+                        parameters: {
+                            type: "OBJECT",
+                            properties: {
+                                country: {
+                                    type: "STRING",
+                                    description: "País para o qual se quer saber a temperatura"
+                                },
+                                isCelsius: {
+                                    type: "BOOLEAN",
+                                    description: "Se devemos retornar em celsius ou não (padrão é true)"
+                                }
+                            },
+                            required: ["country", "isCelsius"]
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+});
+
+console.log(response.candidates[0].content.parts[0].functionCall);
+
+/*
+contents.push(response.candidates[0].content.parts);
+
+contents.push({
+    role: "user",
+    parts: [{
+        functionResponse: {
+            name: "getTodayDate",
+            response: { result: "2025-04-01" }
+        }
+    }]
+});
+
+
+response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: contents,
+});
+
+console.log(response.candidates[0].content);*/
