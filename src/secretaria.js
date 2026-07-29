1500c81b933be70527b3aaeb12510afe1cb52638
FMagalhaes25 <felipemostaco05@gmail.com>
2026-07-29 11:25:09 -0300

    feat: connect tool definitions

--- src/secretaria.js
+++ src/secretaria.js
@@ -1,15 +1,17 @@
 import {GoogleGenAI} from '@google/genai';
-import { allFunctions as calendarFunctions} from './tools/calendar.js';
-import { allFunctions as emailFunctions} from './tools/email.js';
+import { allDefinitions as calendarDefinitions} from './tools/calendar.js';
+import { allDefinitions as emailDefinitions} from './tools/email.js';
+
+const allDefinitions = calendarDefinitions.concat(emailDefinitions);
+const allDeclarations = allDefinitions.map(def => def.declaration);
+const allFunctions = Object.fromEntries(allDefinitions.map(def => [def.declaration.name, def.function]));
 
 const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GEN_AI_API_KEY });
-
-const allFunctions = calendarFunctions.concat(emailFunctions);
 
 const contents = [
     {
         role: "user",
-        parts: [{ text: "Marque um evento novo para o dia 31/12/2036, chamado Casamento com a Manu às 22h" }]
+        parts: [{ text: "Voce pode ver os meus eventos hoje?" }]
     }
 ];
 
@@ -19,7 +21,40 @@ var response = await ai.models.generateContent({
     config: {
         tools: [
             {
-                functionDeclarations: allFunctions
+                functionDeclarations: allDeclarations
+            }
+        ]
+    }
+});
+
+const functionCall = response.candidates[0].content.parts[0].functionCall;
+
+const functionToExecute = functionCall.name;
+const functionParameters = functionCall.args;
+
+const fn = allFunctions[functionToExecute];
+
+const result = fn(functionParameters);
+
+const functionResponse = {
+    role: "user",
+    parts: [{
+        functionResponse: {
+            name: functionToExecute,
+            response: {result: result}
+        }
+    }],
+};
+
+contents.push(functionResponse);
+
+response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: contents,
+    config: {
+        tools: [
+            {
+                functionDeclarations: allDeclarations
             }
         ]
     }
