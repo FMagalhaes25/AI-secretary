a0594edd1aa3a90210d5363d701cd3d184026131
FMagalhaes25 <felipemostaco05@gmail.com>
2026-07-29 10:41:01 -0300

    feat: connect calendar and email tools

--- src/secretaria.js
+++ src/secretaria.js
@@ -1,11 +1,15 @@
 import {GoogleGenAI} from '@google/genai';
+import { allFunctions as calendarFunctions} from './tools/calendar.js';
+import { allFunctions as emailFunctions} from './tools/email.js';
 
 const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GEN_AI_API_KEY });
+
+const allFunctions = calendarFunctions.concat(emailFunctions);
 
 const contents = [
     {
         role: "user",
-        parts: [{ text: "qual a temperatura no Brasil?" }]
+        parts: [{ text: "Marque um evento novo para o dia 31/12/2036, chamado Casamento com a Manu às 22h" }]
     }
 ];
 
@@ -15,51 +19,10 @@ var response = await ai.models.generateContent({
     config: {
         tools: [
             {
-                functionDeclarations: [
-                    {name: "getTodayDate", description: "retorna a data de hoje no formato escrito sem letras"},
-                    {   
-                        name: "getCountryTemperature",
-                        description: "Retorna a temperatura do país indicado",
-                        parameters: {
-                            type: "OBJECT",
-                            properties: {
-                                country: {
-                                    type: "STRING",
-                                    description: "País para o qual se quer saber a temperatura"
-                                },
-                                isCelsius: {
-                                    type: "BOOLEAN",
-                                    description: "Se devemos retornar em celsius ou não (padrão é true)"
-                                }
-                            },
-                            required: ["country", "isCelsius"]
-                        }
-                    }
-                ]
+                functionDeclarations: allFunctions
             }
         ]
     }
 });
 
-console.log(response.candidates[0].content.parts[0].functionCall);
-
-/*
-contents.push(response.candidates[0].content.parts);
-
-contents.push({
-    role: "user",
-    parts: [{
-        functionResponse: {
-            name: "getTodayDate",
-            response: { result: "2025-04-01" }
-        }
-    }]
-});
-
-
-response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: contents,
-});
-
-console.log(response.candidates[0].content);*/
+console.log(response.candidates[0].content.parts[0]);
